32dbe804d36c19b1d466d9ec4ffa5306c74714c2
Brian Kemper <bkemper@gmail.com>
2026-01-06 14:20:09 -0700

    Fix for deploy (#17)

--- family-chores/src/main.tsx
+++ family-chores/src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dak/family-chores">
       <App />
     </BrowserRouter>
   </StrictMode>,
