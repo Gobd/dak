be950b1e76fec2aed8bab6abb77d7e71ec3d71ef
Brian Kemper <bkemper@gmail.com>
2026-01-17 13:39:34 -0700

    .

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ command, mode }) => ({
   // Use /dashboard/ base only for production build, not preview
