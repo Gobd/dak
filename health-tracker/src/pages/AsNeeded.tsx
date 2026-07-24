41c6ddfa87988e98a05392ef9a0e86d439d70f0a
Brian Kemper <bkemper@gmail.com>
2026-07-24 10:07:13 -0600

    .

--- health-tracker/src/pages/AsNeeded.tsx
+++ health-tracker/src/pages/AsNeeded.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useInterval } from '@dak/hooks';
 import { usePeopleStore } from '../stores/people-store';
 import { usePrnStore } from '../stores/prn-store';
